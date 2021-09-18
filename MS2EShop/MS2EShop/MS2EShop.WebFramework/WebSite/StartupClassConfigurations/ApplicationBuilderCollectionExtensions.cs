0005bd58a63072c3c6d0903e700cb2c27163fe95
MohsenS2 <msad137@gmail.com>
2021-09-19 00:50:23 +0430

    Add AdminLayout page

---
+++ MS2EShop/MS2EShop/MS2EShop.WebFramework/WebSite/StartupClassConfigurations/ApplicationBuilderCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+
+namespace MS2EShop.WebFramework.WebSite.StartupClassConfigurations
+{
+    public static class ApplicationBuilderCollectionExtensions
+    {
+        public static void UseWebSiteEndpoints(this IApplicationBuilder app)
+        {
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
---
+++ MS2EShop/MS2EShop/MS2EShop.WebSite/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace MS2EShop.WebSite.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
